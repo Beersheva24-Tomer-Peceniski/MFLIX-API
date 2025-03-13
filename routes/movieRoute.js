35f7acd203a2648c82baf58b51bee068e221d455
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-03-13 15:59:54 +0200

    Create application logger

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { ObjectId } from "mongodb";
 import movieService from "../services/MovieServices.js";
+import appLogger from "../logger/appLogger.js";
 
 const movieRoute = express.Router();
 
 movieRoute.get("/:id", async (req, res) => {
+    appLogger.info("Get movie from ID route requested");
     const movieId = new ObjectId(req.params.id);
     const movie = await movieService.getMovieById(movieId) 
     res.send(movie);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import movieRoute from "../routes/MovieRoute.js";
+import appLogger from "../logger/appLogger.js";
 
 configDotenv()
 const port = process.env.PORT || 4000;
@@ -10,4 +11,4 @@ const app = express();
 app.use(express.json());
 app.use("/movies", movieRoute);
 
-app.listen(port, () => console.log(`Server is listening on the port ${port}`))
+app.listen(port, () => appLogger.info(`Server is listening on the port ${port}`))
